ebb79a1a2f0ae82778616404b26ce5a910304fca
severjason <ssever2010@gmail.com>
2020-04-06 17:05:56 +0300

    first commit for task-app with CRUD operations

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('./db/mongoose');
+const usersRouter = require('./routers/users');
+const tasksRouter = require('./routers/tasks');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(usersRouter);
+app.use(tasksRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is up on port ${PORT}`);
+});
