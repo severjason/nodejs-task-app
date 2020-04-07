14bec171392697959b2d766f0479513a40339bb7
severjason <ssever2010@gmail.com>
2020-04-07 18:43:18 +0300

    added user login/logout endpoints

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('user', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -9,6 +11,7 @@ const User = mongoose.model('user', {
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -37,7 +40,48 @@ const User = mongoose.model('user', {
         throw new Error('Age must be a positive number')
       }
     }
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true,
+    }
+  }]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token =  jwt.sign({_id: user._id.toString()}, 'secretkey');
+
+  user.tokens = [...user.tokens, {token}];
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({email});
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+  return user;
+};
+
+// has the plain text password before saving
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 const User = require('../models/user');
 
 router.patch('/users/:id', async (req, res) => {
@@ -10,11 +11,24 @@ router.patch('/users/:id', async (req, res) => {
     return res.status(400).send({error: 'Invalid updates!'})
   }
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    const user = await User.findById(req.params.id);
     if (!user) {
       return res.status(404).send();
     }
+    updates.forEach(update => user[update] = req.body[update]);
+    await user.save();
     res.send(user);
+  } catch(e) {
+    res.status(400).send(e);
+  }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.send({user, token});
   } catch(e) {
     res.status(400).send(e);
   }
@@ -24,16 +38,32 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({user, token});
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.get('/users', async (req, res ) => {
+router.get('/users/me', auth, async (req, res ) => {
+  res.send(req.user);
+});
+
+router.post('/users/logout', auth, async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+    await req.user.save();
+    res.send();
+  } catch(e) {
+    res.status(500).send(e);
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
   } catch(e) {
     res.status(500).send(e);
   }
